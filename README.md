8070f94679d30c477a0e737817ac3ca6a552a44c
Kyle Simpson <getify@gmail.com>
2013-07-06 20:04:29 -0500

    first steps of rewrite, changing from '' to 'ASQ' branding

--- README.md
+++ README.md
@@ -1,28 +1,28 @@
-# asyncSteps.js
+# asynquence
 
-A lightweight API for performing serial async tasks ("steps"), each in succession.
+A lightweight API for asynchronous sequences and gates.
 
 ## Explanation
 
-Say you want do two or more asynchronous tasks one after the other (like animation delays, XHR calls, etc). You want to set up an ordered sequence of tasks and make sure the previous one finishes before the next one is processed. You need a step chain.
+Say you want do two or more asynchronous tasks one after the other (like animation delays, XHR calls, etc). You want to set up an ordered sequence of tasks and make sure the previous one finishes before the next one is processed. You need a sequence.
 
-You create a step chain by calling `$AS(...)`. **Each time you call `$AS()`, you create a new, separate step chain.**
+You create a sequence by calling `$AS(...)`. **Each time you call `$AS()`, you create a new, separate sequence.**
 
 To create a new step, simply call `then(...)` with a function (or multiple functions, see below). That function will be executed when that step is ready to be processed, and it will be passed as its first parameter the completion trigger. Subsequent parameters, if any, will be any messages passsed on from the immediately previous step.
 
 The completion trigger that your step function(s) receive can be called directly to indicate success, or you can add the `fail` flag (see examples below) to indicate failure of that step. In either case, you can pass one or more messages onto the next step (or the next failure handler) by simply adding them as parameters to the call.
 
-If you register a step using `then(...)` on a step chain which is already currently complete, that step will be processed immediately. Otherwise, calls to `then(...)` will be queued up until that step is ready for processing.
+If you register a step using `then(...)` on a sequence which is already currently complete, that step will be processed immediately. Otherwise, calls to `then(...)` will be queued up until that step is ready for processing.
 
 You can register multiple steps, and multiple failure handlers. However, messages from a previous step (success or failure completion) will only be passed to the immediately next registered step (or the next failure handler). If you want to propagate along a message through multiple steps, you must do so yourself by making sure you re-pass the received message at each step completion.
 
-To listen for any step failing, call `or(...)` on your step chain to register a failure callback. You can call `or()` as many times as you would like. If you call `or()` on a step chain that has already been flagged as failed, the callback you specify will just be executed immediately.
+To listen for any step failing, call `or(...)` on your sequence to register a failure callback. You can call `or()` as many times as you would like. If you call `or()` on a sequence that has already been flagged as failed, the callback you specify will just be executed immediately.
 
 Passing in multiple functions to `$AS(...)` or `then(...)` creates an [implicit async parallel gate (aka asyncGate.js)](http://github.com/getify/asyncGate.js) across those functions, such that the single step in question isn't complete until all segments of the parallel gate are complete.
 
 For implicit parallel gate steps, each segment of that gate will receive a copy of the message(s) passed from the previous step. Also, all messages from the segments of this gate will be passed along to the next step (or the next failure handler, in the case of a gate segment indicating a failure).
 
-You can also `abort()` a step chain at any time, which will prevent any further actions from occurring on that step chain (all callbacks will be ignored). The call to `abort()` can happen on the step chain API itself, or using the `abort` flag on a completion callback in any step (see example below).
+You can also `abort()` a sequence at any time, which will prevent any further actions from occurring on that sequence (all callbacks will be ignored). The call to `abort()` can happen on the sequence API itself, or using the `abort` flag on a completion callback in any step (see example below).
 
 **NOTE: this code explicitly depends on [asyncGate.js](http://github.com/getify/asyncGate.js).** Either "ag.js" (or "ag.src.js") must be present prior to including "as.js" (or "as.src.js"), or you must bundle the two together in the proper order.
 
@@ -79,7 +79,7 @@ Handle step failure:
         },1000);
     })
     .then(function(){
-        // step chain fails, won't ever get called
+        // sequence fails, won't ever get called
     })
     .or(function(msg1,msg2){
         alert("Failure: " + msg1 + " " + msg2); // 'Failure: hello world'
@@ -112,14 +112,14 @@ Create a step that's an [implicit async parallel gate (aka asyncGate.js)](http:/
         alert("Greeting: " + msg2[0] + " " + msg2[1]); // 'Greeting: hello mikey'
     });
 
-Abort a step chain in progress:
+Abort a sequence in progress:
 
     var steps = $AS()
     .then(fn1)
     .then(fn2)
     .then(yay);
     setTimeout(function(){
-        steps.abort(); // will stop the step chain before running steps `fn2` and `yay`
+        steps.abort(); // will stop the sequence before running steps `fn2` and `yay`
     },100);
     
     // same as above
@@ -127,7 +127,7 @@ Abort a step chain in progress:
     .then(fn1)
     .then(function(done){
         setTimeout(function(){
-            done.abort(); // `abort` flag will stop the step chain before running steps `fn2` and `yay`
+            done.abort(); // `abort` flag will stop the sequence before running steps `fn2` and `yay`
         },100);
     })
     .then(fn2)
--- as.src.js
+++ as.src.js
@@ -1,231 +1,37 @@
-/*! asyncSteps.js
-    v0.2 (c) Kyle Simpson
+/*! asynquence
+    v0.0.1 (c) Kyle Simpson
     MIT License: http://getify.mit-license.org
 */
 
 (function(global){
 
-  var old_$AS = global.$AS;
+	var old_ASQ = global.ASQ;
 
-  // test for array
-  function is_array(arr) { return Object.prototype.toString.call(arr) == "[object Array]"; }
-  
-  // flatten array
-  function flatten_array(arr) {
-    for (var i=0; i<arr.length; ) {
-      if (is_array(arr[i])) {
-        // prepend `splice()` arguments to `tmp` array, to enable `apply()` call
-        arr.splice.apply(arr,[i,1].concat(arr[i]));
-        continue;
-      }
-      i++;
-    }
-    
-    return arr;
-  }
+	// test for array
+	function is_array(arr) { return Object.prototype.toString.call(arr) == "[object Array]"; }
 
-  function create_sandbox() {
-    var instanceAPI;
-    
-    instanceAPI = function() {
-      function do_then_queue() {
-        var next, i, _msgs;
+	// flatten array
+	function flatten_array(arr) {
+		for (var i=0; i<arr.length; ) {
+			if (is_array(arr[i])) {
+				// prepend `splice()` arguments to `tmp` array, to enable `apply()` call
+				arr.splice.apply(arr,[i,1].concat(arr[i]));
+				continue;
+			}
+			i++;
+		}
 
-        if (aborted) return;
+		return arr;
+	}
 
-        if (then_queue.length) {
-          _msgs = msgs.slice();
-          msgs = [];
-          next = then_queue.shift();
-          current_gate = $AG();
-          for (i=0; i<next.length; i++) {
-            (function(fn){
-              current_gate.and(function(done){
-                var args = _msgs.slice();
-                done.abort = function(){
-                  chainAPI.abort();
-                };
-                args.unshift(done);
-                fn.apply(fn,args);
-              });
-            })(next[i]);
-          }
-          current_gate.then(gate_finish).or(gate_error);
-          _msgs = null;
-        }
-        else {
-          current_gate.abort();
-          current_gate = null;
-        }
-      }
+	function create_sandbox() {
+		var instanceAPI;
 
-      function do_or_queue(){
-        var fn;
+		instanceAPI = {};
 
-        if (aborted) return;
+		return instanceAPI;
+	}
 
-        // reset the success queue
-        then_queue = true;
-        
-        // make sure at least one error callback is registered
-        if (or_queue !== true && or_queue.length) {
-          // empty the queue
-          while (or_queue && (fn = or_queue.shift())) {
-            if (msgs.length > 0) {
-              fn.apply({},msgs);
-              msgs = [];
-            }
-            else fn.call({});
-          }
-        }
-
-        or_queue = true; // flag it as complete
-        current_gate.abort();
-        current_gate = null;
-      }
-
-      function gate_finish() {
-        var args = [].slice.call(arguments), i;
-        if (!(chain_error || aborted)) {
-          if (arguments.length) {
-            for (i=0; i<args.length; i++) {
-              if (is_array(args[i]) && args[i].length == 1) msgs.push(args[i][0]);
-              else msgs.push(args[i]);
-            }
-          }
-          do_then_queue();
-        }
-      }
-
-      function gate_error() {
-        var args = [].slice.call(arguments), i;
-        if (!(chain_error || aborted)) {
-          chain_error = true;
-
-          if (arguments.length) {
-            for (i=0; i<args.length; i++) {
-              if (is_array(args[i]) && args[i].length == 1) msgs.push(args[i][0]);
-              else msgs.push(args[i]);
-            }
-          }
-          do_or_queue();
-        }
-      }
-
-      function raw_trigger() {
-        var gate = $AG(), trigger_done = gate.and(), trigger;
-
-        trigger = function() {
-          var args = [].slice.call(arguments), fn;
-
-          fn = function(done){ done.apply({},args); };
-          fn.__wrapper__ = true;
-
-          trigger_done(fn);
-        };
-        trigger.fail = function() {
-          var args = [].slice.call(arguments), fn;
-
-          fn = function(done){ done.fail.apply({},args); };
-          fn.__wrapper__ = true;
-
-          trigger_done(fn);
-        };
-        trigger.abort = function() {
-          chainAPI.abort();
-        };
-
-        gate
-        .and(function(then_done){
-          then_queue.push([function(doneFn){
-            then_done(doneFn);
-          }]);
-        })
-        .then(function(wrapper,done){
-          var tmp;
-          wrapper = wrapper[0];
-          done = done[0];
-          // if the params are switched, swap 'em
-          if (!wrapper.__wrapper__) { tmp = wrapper; wrapper = done; done = tmp; }
-
-          wrapper(done);
-        });
-
-        return trigger;
-      }
-
-      var chainAPI,
-          current_gate,
-          then_queue = [],
-          or_queue = [],
-          msgs = [],
-          chain_error = false,
-          aborted = false
-      ;
-            
-      chainAPI = {
-        then: function(){
-          // has the step chain already error'd? if so, bail
-          if (chain_error || aborted) return chainAPI;
-
-          var args = flatten_array([].slice.call(arguments)), ret;
-
-          // special case: if no arguments provided, return a trigger directly
-          if (args.length == 0) {
-            ret = raw_trigger();
-          }
-          else {
-            then_queue.push(args);
-            ret = chainAPI;
-          }
-          if (!current_gate) {
-            do_then_queue();
-          }
-
-          return ret;
-        },
-        or: function(fn){
-          if (aborted) return chainAPI;
-
-          if (or_queue !== true) {
-            or_queue.push(fn);
-          }
-          else {
-            fn.apply({},msgs);
-            msgs = [];
-          }
-
-          return chainAPI;
-        },
-        abort: function(){
-          aborted = false;
-          if (current_gate) current_gate.abort();
-          current_gate = then_queue = or_queue = msgs = null;
-        }
-      };
-      
-      // add in arguments passed as constructor parameters
-      if (arguments.length > 0) chainAPI.then.apply({},arguments);
-      
-      return chainAPI;
-    };
-     
-    // rollback to a previous $AS global ref, if any
-    // and return this copy
-    instanceAPI.noConflict = function() {
-      var new_$AS = global.$AS;
-      global.$AS = old_$AS;
-      return new_$AS;
-    };
-    
-    // create a fresh, clean, sandboxed copy of the current $AS
-    instanceAPI.sandbox = function() {
-      return create_sandbox();
-    };
-    
-    return instanceAPI;
-  }
-  
-  global.$AS = create_sandbox();
+	global.ASQ = create_sandbox();
 
 })(this);
