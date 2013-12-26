63ffa758bc41e83ff7de5656b224a6e5d03fe175
Kyle Simpson <getify@gmail.com>
2013-12-25 21:24:12 -0600

    simplifying internals

--- asq.src.js
+++ asq.src.js
@@ -19,64 +19,38 @@
 		return (typeof setImmediate !== "undefined") ? setImmediate(fn) : setTimeout(fn,0);
 	}
 
-	function createSandbox() {
+	function createSequence() {
 
-		function createSequence() {
+		function resetSequence() {
+			clearTimeout(seq_tick);
+			seq_tick = null;
+			then_queue.length = 0;
+			or_queue.length = 0;
+			sequence_messages.length = 0;
+			sequence_errors.length = 0;
+		}
 
-			function resetSequence() {
-				clearTimeout(seq_tick);
-				seq_tick = null;
-				then_queue.length = 0;
-				or_queue.length = 0;
-				sequence_messages.length = 0;
-				sequence_errors.length = 0;
+		function scheduleSequenceTick() {
+			if (seq_aborted) return sequenceTick();
+
+			if (!seq_tick) {
+				seq_tick = schedule(sequenceTick);
 			}
+		}
 
-			function scheduleSequenceTick() {
-				if (seq_aborted) return sequenceTick();
+		function sequenceTick() {
+			var fn, args;
 
-				if (!seq_tick) {
-					seq_tick = schedule(sequenceTick);
-				}
+			seq_tick = null;
+
+			if (seq_aborted) {
+				resetSequence();
 			}
-
-			function sequenceTick() {
-				var fn, args;
-
-				seq_tick = null;
-
-				if (seq_aborted) {
-					resetSequence();
-				}
-				else if (seq_error) {
-					while (or_queue.length) {
-						fn = or_queue.shift();
-						try {
-							fn.apply(fn,sequence_errors);
-						}
-						catch (err) {
-							if (checkBranding(err)) {
-								sequence_errors = sequence_errors.concat(err);
-							}
-							else {
-								sequence_errors.push(err);
-								if (err.stack) sequence_errors.push(err.stack);
-							}
-							if (or_queue.length === 0) {
-								console.error.apply(console,sequence_errors);
-							}
-						}
-					}
-				}
-				else if (then_ready && then_queue.length > 0) {
-					then_ready = false;
-					fn = then_queue.shift();
-					args = sequence_messages.slice();
-					sequence_messages.length = 0;
-					args.unshift(createStepCompletion());
-
+			else if (seq_error) {
+				while (or_queue.length) {
+					fn = or_queue.shift();
 					try {
-						fn.apply(fn,args);
+						fn.apply(fn,sequence_errors);
 					}
 					catch (err) {
 						if (checkBranding(err)) {
@@ -84,327 +58,348 @@
 						}
 						else {
 							sequence_errors.push(err);
+							if (err.stack) sequence_errors.push(err.stack);
 						}
-						seq_error = true;
-						scheduleSequenceTick();
+						if (or_queue.length === 0) {
+							console.error.apply(console,sequence_errors);
+						}
 					}
 				}
 			}
+			else if (then_ready && then_queue.length > 0) {
+				then_ready = false;
+				fn = then_queue.shift();
+				args = sequence_messages.slice();
+				sequence_messages.length = 0;
+				args.unshift(createStepCompletion());
 
-			function createStepCompletion() {
-				function done() {
-					// ignore this call?
-					if (seq_error || seq_aborted || then_ready) return;
-
-					then_ready = true;
-					sequence_messages.push.apply(sequence_messages,arguments);
-					sequence_errors.length = 0;
-
-					scheduleSequenceTick();
+				try {
+					fn.apply(fn,args);
 				}
-
-				done.fail = function(){
-					// ignore this call?
-					if (seq_error || seq_aborted || then_ready) return;
-
+				catch (err) {
+					if (checkBranding(err)) {
+						sequence_errors = sequence_errors.concat(err);
+					}
+					else {
+						sequence_errors.push(err);
+					}
 					seq_error = true;
-					sequence_messages.length = 0;
-					sequence_errors.push.apply(sequence_errors,arguments);
-
 					scheduleSequenceTick();
-				};
+				}
+			}
+		}
 
-				done.abort = function(){
-					if (seq_error || seq_aborted) return;
+		function createStepCompletion() {
+			function done() {
+				// ignore this call?
+				if (seq_error || seq_aborted || then_ready) return;
 
-					then_ready = false;
-					seq_aborted = true;
-					sequence_messages.length = 0;
-					sequence_errors.length = 0;
+				then_ready = true;
+				sequence_messages.push.apply(sequence_messages,arguments);
+				sequence_errors.length = 0;
 
-					scheduleSequenceTick();
-				};
-
-				return done;
+				scheduleSequenceTick();
 			}
 
-			function createGate(stepCompletion,segments,seqMessages) {
+			done.fail = function(){
+				// ignore this call?
+				if (seq_error || seq_aborted || then_ready) return;
 
-				function resetGate() {
-					clearTimeout(gate_tick);
-					gate_tick = segment_completion = segment_messages = segment_error_message = null;
+				seq_error = true;
+				sequence_messages.length = 0;
+				sequence_errors.push.apply(sequence_errors,arguments);
+
+				scheduleSequenceTick();
+			};
+
+			done.abort = function(){
+				if (seq_error || seq_aborted) return;
+
+				then_ready = false;
+				seq_aborted = true;
+				sequence_messages.length = 0;
+				sequence_errors.length = 0;
+
+				scheduleSequenceTick();
+			};
+
+			return done;
+		}
+
+		function createGate(stepCompletion,segments,seqMessages) {
+
+			function resetGate() {
+				clearTimeout(gate_tick);
+				gate_tick = segment_completion = segment_messages = segment_error_message = null;
+			}
+
+			function scheduleGateTick() {
+				if (gate_aborted) return gateTick();
+
+				if (!gate_tick) {
+					gate_tick = schedule(gateTick);
 				}
+			}
 
-				function scheduleGateTick() {
-					if (gate_aborted) return gateTick();
+			function gateTick() {
+				if (seq_error || seq_aborted || gate_completed) return;
 
-					if (!gate_tick) {
-						gate_tick = schedule(gateTick);
-					}
+				var args = [];
+
+				gate_tick = null;
+
+				if (gate_error) {
+					stepCompletion.fail.apply(stepCompletion,segment_error_message);
+
+					resetGate();
 				}
+				else if (gate_aborted) {
+					stepCompletion.abort();
 
-				function gateTick() {
-					if (seq_error || seq_aborted || gate_completed) return;
-
-					var args = [];
-
-					gate_tick = null;
-
-					if (gate_error) {
-						stepCompletion.fail.apply(stepCompletion,segment_error_message);
-
-						resetGate();
-					}
-					else if (gate_aborted) {
-						stepCompletion.abort();
-
-						resetGate();
-					}
-					else if (checkGate()) {
-						gate_completed = true;
-
-						// collect all the messages from the gate segments
-						segment_completion.forEach(function(sc,i){
-							args.push(segment_messages["s" + i]);
-						});
-
-						stepCompletion.apply(stepCompletion,args);
-
-						resetGate();
-					}
+					resetGate();
 				}
+				else if (checkGate()) {
+					gate_completed = true;
 
-				function checkGate() {
-					if (seq_error || seq_aborted || gate_error || gate_aborted || gate_completed || segment_completion.length === 0) return;
-
-					var fulfilled = true;
-
-					segment_completion.some(function(segcom){
-						if (segcom === null) {
-							fulfilled = false;
-							return true; // break
-						}
+					// collect all the messages from the gate segments
+					segment_completion.forEach(function(sc,i){
+						args.push(segment_messages["s" + i]);
 					});
 
-					return fulfilled;
+					stepCompletion.apply(stepCompletion,args);
+
+					resetGate();
 				}
+			}
 
-				function createSegmentCompletion() {
+			function checkGate() {
+				if (seq_error || seq_aborted || gate_error || gate_aborted || gate_completed || segment_completion.length === 0) return;
 
-					function done() {
-						// ignore this call?
-						if (seq_error || seq_aborted || gate_error ||
-							gate_aborted || gate_completed ||
-							segment_completion[segment_completion_idx]
-						) {
-							return;
-						}
+				var fulfilled = true;
 
-						// put gate-segment messages into `messages`-branded container
-						var args = public_api.messages.apply(null,arguments);
-
-						segment_messages["s" + segment_completion_idx] = args.length > 1 ? args : args[0];
-						segment_completion[segment_completion_idx] = true;
-
-						scheduleGateTick();
-					}
-
-					var segment_completion_idx = segment_completion.length;
-
-					done.fail = function(){
-						// ignore this call?
-						if (seq_error || seq_aborted || gate_error ||
-							gate_aborted || gate_completed ||
-							segment_completion[segment_completion_idx]
-						) {
-							return;
-						}
-
-						gate_error = true;
-						segment_error_message = ARRAY_SLICE.call(arguments);
-
-						scheduleGateTick();
-					};
-
-					done.abort = function(){
-						if (seq_error || seq_aborted || gate_error || gate_aborted || gate_completed) return;
-
-						gate_aborted = true;
-
-						gateTick(); // abort() is an immediate/synchronous action
-					};
-
-					// placeholder for when a gate-segment completes
-					segment_completion[segment_completion_idx] = null;
-
-					return done;
-				}
-
-				var gate_error = false,
-					gate_aborted = false,
-					gate_completed = false,
-
-					args,
-					err_msg,
-
-					segment_completion = [],
-					segment_messages = {},
-					segment_error_message,
-
-					gate_tick
-				;
-
-				segments.some(function(seg){
-					if (gate_error || gate_aborted) return true; // break
-
-					args = seqMessages.slice();
-					args.unshift(createSegmentCompletion());
-					try {
-						seg.apply(seg,args);
-					}
-					catch (err) {
-						err_msg = err;
-						gate_error = true;
+				segment_completion.some(function(segcom){
+					if (segcom === null) {
+						fulfilled = false;
 						return true; // break
 					}
 				});
 
-				if (err_msg) {
-					if (checkBranding(err_msg)) {
-						stepCompletion.fail.apply(null,err_msg);
+				return fulfilled;
+			}
+
+			function createSegmentCompletion() {
+
+				function done() {
+					// ignore this call?
+					if (seq_error || seq_aborted || gate_error ||
+						gate_aborted || gate_completed ||
+						segment_completion[segment_completion_idx]
+					) {
+						return;
 					}
-					else {
-						stepCompletion.fail(err_msg);
-					}
+
+					// put gate-segment messages into `messages`-branded container
+					var args = public_api.messages.apply(null,arguments);
+
+					segment_messages["s" + segment_completion_idx] = args.length > 1 ? args : args[0];
+					segment_completion[segment_completion_idx] = true;
+
+					scheduleGateTick();
 				}
+
+				var segment_completion_idx = segment_completion.length;
+
+				done.fail = function(){
+					// ignore this call?
+					if (seq_error || seq_aborted || gate_error ||
+						gate_aborted || gate_completed ||
+						segment_completion[segment_completion_idx]
+					) {
+						return;
+					}
+
+					gate_error = true;
+					segment_error_message = ARRAY_SLICE.call(arguments);
+
+					scheduleGateTick();
+				};
+
+				done.abort = function(){
+					if (seq_error || seq_aborted || gate_error || gate_aborted || gate_completed) return;
+
+					gate_aborted = true;
+
+					gateTick(); // abort() is an immediate/synchronous action
+				};
+
+				// placeholder for when a gate-segment completes
+				segment_completion[segment_completion_idx] = null;
+
+				return done;
 			}
 
-			function then() {
-				if (seq_error || seq_aborted || arguments.length === 0) return sequence_api;
+			var gate_error = false,
+				gate_aborted = false,
+				gate_completed = false,
 
-				then_queue.push.apply(then_queue,arguments);
+				args,
+				err_msg,
 
-				scheduleSequenceTick();
+				segment_completion = [],
+				segment_messages = {},
+				segment_error_message,
 
-				return sequence_api;
-			}
-
-			function or() {
-				if (seq_aborted || arguments.length === 0) return sequence_api;
-
-				or_queue.push.apply(or_queue,arguments);
-
-				scheduleSequenceTick();
-
-				return sequence_api;
-			}
-
-			function gate() {
-				if (seq_error || seq_aborted || arguments.length === 0) return sequence_api;
-
-				var fns = ARRAY_SLICE.call(arguments);
-
-				then(function(done){
-					var args = ARRAY_SLICE.call(arguments,1);
-					createGate(done,fns,args);
-				});
-
-				return sequence_api;
-			}
-
-			function pipe() {
-				if (seq_aborted || arguments.length === 0) return sequence_api;
-
-				ARRAY_SLICE.call(arguments).forEach(function(fn){
-					then(function(done){
-						fn.apply(fn,ARRAY_SLICE.call(arguments,1));
-						done();
-					})
-					.or(fn.fail);
-				});
-
-				return sequence_api;
-			}
-
-			function seq() {
-				if (seq_error || seq_aborted || arguments.length === 0) return sequence_api;
-
-				ARRAY_SLICE.call(arguments).forEach(function(fn){
-					then(function(done){
-						// check if this argument is not already an ASQ instance?
-						// if not, assume a function to invoke that will return an ASQ instance
-						if (!checkBranding(fn)) {
-							fn = fn.apply(fn,ARRAY_SLICE.call(arguments,1));
-						}
-						// now, pipe the ASQ instance into our current sequence
-						fn.pipe(done);
-					});
-				});
-
-				return sequence_api;
-			}
-
-			function val() {
-				if (seq_error || seq_aborted || arguments.length === 0) return sequence_api;
-
-				ARRAY_SLICE.call(arguments).forEach(function(fn){
-					then(function(done){
-						var msgs = fn.apply(fn,ARRAY_SLICE.call(arguments,1));
-						if (!checkBranding(msgs)) {
-							msgs = public_api.messages.call(null,msgs);
-						}
-						done.apply(done,msgs);
-					});
-				});
-
-				return sequence_api;
-			}
-
-			function abort() {
-				if (seq_error) return sequence_api;
-
-				seq_aborted = true;
-
-				sequenceTick();
-
-				return sequence_api;
-			}
-
-
-			var seq_error = false,
-				seq_aborted = false,
-				then_ready = true,
-
-				then_queue = [],
-				or_queue = [],
-
-				sequence_messages = [],
-				sequence_errors = [],
-
-				seq_tick,
-
-				// brand the sequence API so we can detect ASQ instances
-				sequence_api = brandIt({
-					then: then,
-					or: or,
-					gate: gate,
-					pipe: pipe,
-					seq: seq,
-					val: val,
-					abort: abort
-				})
+				gate_tick
 			;
 
-			// treat ASQ() constructor parameters as having been passed to `then()`
-			sequence_api.then.apply(sequence_api,arguments);
+			segments.some(function(seg){
+				if (gate_error || gate_aborted) return true; // break
+
+				args = seqMessages.slice();
+				args.unshift(createSegmentCompletion());
+				try {
+					seg.apply(seg,args);
+				}
+				catch (err) {
+					err_msg = err;
+					gate_error = true;
+					return true; // break
+				}
+			});
+
+			if (err_msg) {
+				if (checkBranding(err_msg)) {
+					stepCompletion.fail.apply(null,err_msg);
+				}
+				else {
+					stepCompletion.fail(err_msg);
+				}
+			}
+		}
+
+		function then() {
+			if (seq_error || seq_aborted || arguments.length === 0) return sequence_api;
+
+			then_queue.push.apply(then_queue,arguments);
+
+			scheduleSequenceTick();
 
 			return sequence_api;
 		}
 
-		return createSequence;
+		function or() {
+			if (seq_aborted || arguments.length === 0) return sequence_api;
+
+			or_queue.push.apply(or_queue,arguments);
+
+			scheduleSequenceTick();
+
+			return sequence_api;
+		}
+
+		function gate() {
+			if (seq_error || seq_aborted || arguments.length === 0) return sequence_api;
+
+			var fns = ARRAY_SLICE.call(arguments);
+
+			then(function(done){
+				var args = ARRAY_SLICE.call(arguments,1);
+				createGate(done,fns,args);
+			});
+
+			return sequence_api;
+		}
+
+		function pipe() {
+			if (seq_aborted || arguments.length === 0) return sequence_api;
+
+			ARRAY_SLICE.call(arguments).forEach(function(fn){
+				then(function(done){
+					fn.apply(fn,ARRAY_SLICE.call(arguments,1));
+					done();
+				})
+				.or(fn.fail);
+			});
+
+			return sequence_api;
+		}
+
+		function seq() {
+			if (seq_error || seq_aborted || arguments.length === 0) return sequence_api;
+
+			ARRAY_SLICE.call(arguments).forEach(function(fn){
+				then(function(done){
+					// check if this argument is not already an ASQ instance?
+					// if not, assume a function to invoke that will return an ASQ instance
+					if (!checkBranding(fn)) {
+						fn = fn.apply(fn,ARRAY_SLICE.call(arguments,1));
+					}
+					// now, pipe the ASQ instance into our current sequence
+					fn.pipe(done);
+				});
+			});
+
+			return sequence_api;
+		}
+
+		function val() {
+			if (seq_error || seq_aborted || arguments.length === 0) return sequence_api;
+
+			ARRAY_SLICE.call(arguments).forEach(function(fn){
+				then(function(done){
+					var msgs = fn.apply(fn,ARRAY_SLICE.call(arguments,1));
+					if (!checkBranding(msgs)) {
+						msgs = public_api.messages.call(null,msgs);
+					}
+					done.apply(done,msgs);
+				});
+			});
+
+			return sequence_api;
+		}
+
+		function abort() {
+			if (seq_error) return sequence_api;
+
+			seq_aborted = true;
+
+			sequenceTick();
+
+			return sequence_api;
+		}
+
+
+		var seq_error = false,
+			seq_aborted = false,
+			then_ready = true,
+
+			then_queue = [],
+			or_queue = [],
+
+			sequence_messages = [],
+			sequence_errors = [],
+
+			seq_tick,
+
+			// brand the sequence API so we can detect ASQ instances
+			sequence_api = brandIt({
+				then: then,
+				or: or,
+				gate: gate,
+				pipe: pipe,
+				seq: seq,
+				val: val,
+				abort: abort
+			})
+		;
+
+		// treat ASQ() constructor parameters as having been passed to `then()`
+		sequence_api.then.apply(sequence_api,arguments);
+
+		return sequence_api;
 	}
 
-	public_api = createSandbox();
+	public_api = createSequence();
 
 	public_api.messages = function() {
 		var ret = ARRAY_SLICE.call(arguments);
