cdc5afcffaeab4cb137f2bc93628be5a46fab923
Kyle Simpson <getify@gmail.com>
2015-07-06 06:47:49 -0500

    contrib: fixing 'none(..)' chainability with reactive sequences, per #61

--- contrib/plugin.none.js
+++ contrib/plugin.none.js
@@ -7,75 +7,81 @@ ASQ.extend("none",function __extend__(api,internals){
 			return api;
 		}
 
-		function reset() {
-			finished = true;
-			error_messages.length = 0;
-			success_messages.length = 0;
-		}
+		var fns = ARRAY_SLICE.call(arguments);
 
-		function complete(trigger) {
-			if (success_messages.length > 0) {
-				// any successful segment's message(s) sent
-				// to main sequence to proceed as **error**
-				success_messages.length = fns.length;
-				trigger.fail.apply(ø,success_messages);
-			}
-			else {
-				// send errors as **success** to main sequence
-				error_messages.length = fns.length;
-				trigger.apply(ø,error_messages);
+		api.then(function __then__(done){
+
+			function reset() {
+				finished = true;
+				error_messages.length = 0;
+				success_messages.length = 0;
 			}
 
-			reset();
-		}
+			function complete(trigger) {
+				if (success_messages.length > 0) {
+					// any successful segment's message(s) sent
+					// to main sequence to proceed as **error**
+					success_messages.length = fns.length;
+					trigger.fail.apply(ø,success_messages);
+				}
+				else {
+					// send errors as **success** to main sequence
+					error_messages.length = fns.length;
+					trigger.apply(ø,error_messages);
+				}
 
-		function success(trigger,idx,args) {
-			if (!finished) {
-				completed++;
-				success_messages[idx] =
-					args.length > 1 ?
-					ASQ.messages.apply(ø,args) :
-					args[0]
-				;
+				reset();
+			}
+
+			function success(trigger,idx,args) {
+				if (!finished) {
+					completed++;
+					success_messages[idx] =
+						args.length > 1 ?
+						ASQ.messages.apply(ø,args) :
+						args[0]
+					;
+
+					// all segments complete?
+					if (completed === fns.length) {
+						finished = true;
+
+						complete(trigger);
+					}
+				}
+			}
+
+			function failure(trigger,idx,args) {
+				if (!finished &&
+					!(idx in error_messages)
+				) {
+					completed++;
+					error_messages[idx] =
+						args.length > 1 ?
+						ASQ.messages.apply(ø,args) :
+						args[0]
+					;
+				}
 
 				// all segments complete?
-				if (completed === fns.length) {
+				if (!finished &&
+					completed === fns.length
+				) {
 					finished = true;
 
 					complete(trigger);
 				}
 			}
-		}
 
-		function failure(trigger,idx,args) {
-			if (!finished &&
-				!(idx in error_messages)
-			) {
-				completed++;
-				error_messages[idx] =
-					args.length > 1 ?
-					ASQ.messages.apply(ø,args) :
-					args[0]
-				;
-			}
+			var completed = 0, error_messages = [], finished = false,
+				sq = ASQ.apply(ø,ARRAY_SLICE.call(arguments,1)),
+				success_messages = []
+			;
 
-			// all segments complete?
-			if (!finished &&
-				completed === fns.length
-			) {
-				finished = true;
+			wrapGate(sq,fns,success,failure,reset);
 
-				complete(trigger);
-			}
-		}
-
-		var completed = 0, error_messages = [], finished = false, fns,
-			success_messages = []
-		;
-
-		fns = ARRAY_SLICE.call(arguments);
-
-		wrapGate(api,fns,success,failure,reset);
+			sq.pipe(done);
+		});
 
 		return api;
 	};
